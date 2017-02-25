6206e9a1e92e3c56e293c7e8a90eb036ceae8087
Malik Saafir <msaafir99@gmail.com>
2017-02-24 21:39:52 -0500

    added array of all the topping query results for use on all the pages

--- connect.php
+++ connect.php
@@ -8,4 +8,11 @@
   if ($connection->connect_error) {
     die("Connection failed: " . $connection->connect_error);
   }
+
+  $results = [
+    'cheeses' => $connection->query("select * from topping where text_id like 'cheese-%'"),
+    'sauces' => $connection->query("select * from topping where text_id like 'sauce-%'"),
+    'meats' => $connection->query("select * from topping where text_id like 'meat-%'"),
+    'fruits' =>$connection->query("select * from topping where text_id like 'fruit-%'")
+  ];
  ?>
