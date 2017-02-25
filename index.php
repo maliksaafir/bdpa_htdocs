6e50a743223d353e4c82b36f315c3f41e39dbf51
Malik Saafir <msaafir99@gmail.com>
2017-02-25 10:51:43 -0500

    quick formatting

--- index.php
+++ index.php
@@ -11,29 +11,29 @@
         <div class="group">
           <label for="cheese">Cheese</label>
           <?php while($row = mysqli_fetch_assoc($results['cheeses'])) { ?>
-          <input name="cheese" type="radio" value="<?php echo $row['name']; ?>">
-          <span><?php echo $row['name']; ?>: $<?php echo $row['price']; ?></span>
+            <input name="cheese" type="radio" value="<?php echo $row['name']; ?>">
+            <span><?php echo $row['name']; ?>: $<?php echo $row['price']; ?></span>
           <?php } ?>
         </div>
         <div class="group">
           <label>Sauce</label>
           <?php while($row = mysqli_fetch_assoc($results['sauces'])) { ?>
-          <input name="sauce" type="radio" value="<?php echo $row['name']; ?>">
-          <span><?php echo $row['name']; ?>: Free</span>
+            <input name="sauce" type="radio" value="<?php echo $row['name']; ?>">
+            <span><?php echo $row['name']; ?>: Free</span>
           <?php } ?>
         </div>
         <div class="group">
           <label>Meat</label>
           <?php while($row = mysqli_fetch_assoc($results['meats'])) { ?>
-          <input name="meat[]" type="checkbox" value="<?php echo $row['name']; ?>">
-          <span><?php echo $row['name']; ?>: $<?php echo $row['price']; ?></span>
+            <input name="meat[]" type="checkbox" value="<?php echo $row['name']; ?>">
+            <span><?php echo $row['name']; ?>: $<?php echo $row['price']; ?></span>
           <?php } ?>
         </div>
         <div class="group">
           <label>Fruits and Veggies</label>
           <?php while($row = mysqli_fetch_assoc($results['fruits'])) { ?>
-          <input name="fruit[]" type="checkbox" value="<?php echo $row['name']; ?>">
-          <span><?php echo $row['name']; ?>: $<?php echo $row['price']; ?></span>
+            <input name="fruit[]" type="checkbox" value="<?php echo $row['name']; ?>">
+            <span><?php echo $row['name']; ?>: $<?php echo $row['price']; ?></span>
           <?php } ?>
         </div>
         <button type="submit">Submit</button>
